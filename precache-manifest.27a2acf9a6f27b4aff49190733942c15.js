93d8474336e1546f0d3f9b33dd1e49615a545796
waelvilla <waelvilla@gmail.com>
2020-05-01 16:08:10 +0300

    Updates

---
+++ precache-manifest.27a2acf9a6f27b4aff49190733942c15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2917195425bae29721f13ae0756451ac",
+    "url": "/educator-webapp/index.html"
+  },
+  {
+    "revision": "dd6a41a6f0a74ccf9667",
+    "url": "/educator-webapp/static/css/main.66934c36.chunk.css"
+  },
+  {
+    "revision": "a71646fbdd1d73d41927",
+    "url": "/educator-webapp/static/js/2.d1a2d9f0.chunk.js"
+  },
+  {
+    "revision": "71633020af3b95917be6b8668e9dacbb",
+    "url": "/educator-webapp/static/js/2.d1a2d9f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6a41a6f0a74ccf9667",
+    "url": "/educator-webapp/static/js/main.84c21985.chunk.js"
+  },
+  {
+    "revision": "133d8980e20ab5cf8447",
+    "url": "/educator-webapp/static/js/runtime-main.de43856e.js"
+  },
+  {
+    "revision": "287167ebcffe0abf05d4c349a0674f58",
+    "url": "/educator-webapp/static/media/ithnain.287167eb.png"
+  }
+]);
