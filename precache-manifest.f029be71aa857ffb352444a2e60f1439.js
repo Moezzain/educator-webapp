fbe7183399633266651238f3e99e50626c75720f
waelvilla <waelvilla@gmail.com>
2020-05-01 23:20:05 +0300

    Updates

---
+++ precache-manifest.f029be71aa857ffb352444a2e60f1439.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415869c86db3e5b637b230585570a33f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e31ddb8fff056832fba",
+    "url": "./static/css/main.66934c36.chunk.css"
+  },
+  {
+    "revision": "a006e59c246c168d6026",
+    "url": "./static/js/2.fa379e53.chunk.js"
+  },
+  {
+    "revision": "71633020af3b95917be6b8668e9dacbb",
+    "url": "./static/js/2.fa379e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e31ddb8fff056832fba",
+    "url": "./static/js/main.fbe62b9d.chunk.js"
+  },
+  {
+    "revision": "9cef202cd61045757a37",
+    "url": "./static/js/runtime-main.5e4f6aa1.js"
+  },
+  {
+    "revision": "287167ebcffe0abf05d4c349a0674f58",
+    "url": "./static/media/ithnain.287167eb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/educator-webapp/precache-manifest.3b3c124ee6bcda621bf92b24c5f0a0b7.js"
+  "./precache-manifest.f029be71aa857ffb352444a2e60f1439.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/educator-webapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
