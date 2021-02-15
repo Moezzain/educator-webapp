33c30820af8615ffe949737255f6bbb0862555d1
waelvilla <waelvilla@gmail.com>
2021-02-15 18:17:29 +0300

    added but didnt finish patient profile

--- src/views/PatientProfile.js
+++ src/views/PatientProfile.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Tab, Col, Row, Form } from 'react-bootstrap'
+import { Tab, Col, Row, Form, Button } from 'react-bootstrap'
 import { DataContext } from "../stateManagement/context";
 
 const defaultProfile = {
@@ -19,174 +19,197 @@ const defaultProfile = {
   haveTakenDiet: '',
 }
 
-const PatientProfile = (props) => {
-  const context = useContext(DataContext)
-  let { chats, hidePatient, showEducators, activeChat } = context;
 
-  if (!chats) {
-    return null
+
+class PatientProfile extends React.Component {
+  static contextType = DataContext
+  state = {
+    editPatient: {
+      medicalProfile: {
+        ...defaultProfile
+      }
+    }
   }
-  console.log('rendering profile');
+  componentDidMount(){
+    
+  }
+  setEditPatient = prop => {
+    let { editPatient } = this.state;
+    editPatient.medicalProfile = { ...editPatient.medicalProfile, ...prop };
+    this.setState({
+      editPatient,
+    });
+  };
 
-  return chats.map(chat => {
-    if (!chat.medicalProfile) {
-       chat.medicalProfile= defaultProfile
+  render() {
+    let { chats, hidePatient, showEducators, activeChat } = this.context;
+
+    if (!chats) {
+      return null
     }
-    let { years,
-      age,
-      weight,
-      height,
-      hba1c,
-      medications,
-      patientName,
-      notes,
-      disease,
-      sex,
-      whoIsPatient,
-      surgery,
-      otherDiseases,
-      haveTakenDiet,
-    } = chat.medicalProfile
+    console.log('rendering profile');
 
-    if (chat.id != activeChat) {
-      return;
-    }
-    return (
-      <Tab.Pane key={chat.id} eventKey={chat.id}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column"
-          }}
-        >
-          <h3>{chat.id}</h3>
+    return chats.map(chat => {
+      if (!chat.medicalProfile) {
+        chat.medicalProfile = defaultProfile
+      }
+      let { years,
+        age,
+        weight,
+        height,
+        hba1c,
+        medications,
+        patientName,
+        notes,
+        disease,
+        sex,
+        whoIsPatient,
+        surgery,
+        otherDiseases,
+        haveTakenDiet,
+      } = chat.medicalProfile
+      console.log('patientName:', chat.patientName);
 
-          <Form style={{ width: '100vh', maxHeight: '75vh', overflowY: 'scroll', overflowX: 'hidden' }}>
-            <Form.Row>
-              <Form.Group as={Col}>
-                <Form.Label column sm="10">
-                  disease
-              </Form.Label>
-                <Col sm="10">
-                  <Form.Control type="text" defaultValue={disease} />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Col}>
-                <Form.Label column sm="10">
-                  years
-              </Form.Label>
-                <Col sm="10">
-                  <Form.Control type="text" defaultValue={years} />
-                </Col>
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col}>
-                <Form.Label column sm="10">
-                  Hba1C
-              </Form.Label>
-                <Col sm="10">
-                  <Form.Control type="text" defaultValue={hba1c} />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Col}>
-                <Form.Label column sm="10">
-                  age
-              </Form.Label>
-                <Col sm="10">
-                  <Form.Control type="text" defaultValue={age} />
-                </Col>
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col}>
-                <Form.Label column sm="10">
-                  sex
-              </Form.Label>
-                <Col sm="10">
-                  <Form.Control type="text" defaultValue={sex} />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Col}>
-                <Form.Label column sm="10">
-                  Patient Type
-              </Form.Label>
-                <Col sm="10">
-                  <Form.Control type="text" defaultValue={whoIsPatient} />
-                </Col>
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col}>
-                <Form.Label column sm="10">
-                  medications
-              </Form.Label>
-                <Col sm="10">
-                  <Form.Control type="text" defaultValue={medications} />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Col}>
-                <Form.Label column sm="10">
-                  otherDiseases
-              </Form.Label>
-                <Col sm="10">
-                  <Form.Control type="text" defaultValue={otherDiseases} />
-                </Col>
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col}>
-                <Form.Label column sm="10">
-                  Weight
-              </Form.Label>
-                <Col sm="10">
-                  <Form.Control type="text" defaultValue={weight} />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Col}>
-                <Form.Label column sm="10">
-                  Height
-              </Form.Label>
-                <Col sm="10">
-                  <Form.Control type="text" defaultValue={height} />
-                </Col>
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
+      if (chat.id != activeChat) {
+        return;
+      }
+      return (
+        <Tab.Pane key={chat.id} eventKey={chat.id}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column"
+            }}
+          >
+            <h3>{chat.id}</h3>
 
-              <Form.Group as={Col}>
-                <Form.Label column sm="10">
-                  Diet Taken
+            <Form style={{ width: '100vh', maxHeight: '75vh', overflowY: 'scroll', overflowX: 'hidden' }}>
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Form.Label column sm="10">
+                    disease
               </Form.Label>
-                <Col sm="10">
-                  <Form.Control type="text" defaultValue={haveTakenDiet} />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Col}>
-                <Form.Label column sm="10">
-                  Past Surgeries
+                  <Col sm="10">
+                    <Form.Control type="text" defaultValue={disease} />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Label column sm="10">
+                    years
               </Form.Label>
-                <Col sm="10">
-                  <Form.Control type="text" defaultValue={surgery} />
-                </Col>
-              </Form.Group>
-            </Form.Row>
-            <Form.Group as={Row}>
-              <Form.Label column sm="10">
-                Notes
+                  <Col sm="10">
+                    <Form.Control type="text" defaultValue={years} />
+                  </Col>
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Form.Label column sm="10">
+                    Hba1C
               </Form.Label>
-              <Col sm="10">
-                <Form.Control as="textarea" rows="3" defaultValue={notes} />
-              </Col>
-            </Form.Group>
-          </Form>
+                  <Col sm="10">
+                    <Form.Control type="text" defaultValue={hba1c} />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Label column sm="10">
+                    age
+              </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="text" defaultValue={age} />
+                  </Col>
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Form.Label column sm="10">
+                    gender
+              </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="text" defaultValue={sex} />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Label column sm="10">
+                    Patient Type
+              </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="text" defaultValue={whoIsPatient} />
+                  </Col>
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Form.Label column sm="10">
+                    medications
+              </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="text" defaultValue={medications} />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Label column sm="10">
+                    otherDiseases
+              </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="text" defaultValue={otherDiseases} />
+                  </Col>
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Form.Label column sm="10">
+                    Weight
+              </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="text" defaultValue={weight} />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Label column sm="10">
+                    Height
+              </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="text" defaultValue={height} />
+                  </Col>
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
 
-        </div>
-      </Tab.Pane>
-    );
-  });
+                <Form.Group as={Col}>
+                  <Form.Label column sm="10">
+                    Diet Taken
+              </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="text" defaultValue={haveTakenDiet} />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Label column sm="10">
+                    Past Surgeries
+              </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="text" defaultValue={surgery} />
+                  </Col>
+                </Form.Group>
+              </Form.Row>
+              <Form.Group as={Row}>
+                <Form.Label column sm="10">
+                  Notes
+              </Form.Label>
+                <Col sm="10">
+                  <Form.Control as="textarea" defaultValue={notes} />
+                </Col>
+              </Form.Group>
+            </Form>
+            <Button variant="success" onClick={() => console.log('clicked')}>Save</Button>
+          </div>
+        </Tab.Pane>
+      );
+    });
+  }
 }
 
 export default PatientProfile
