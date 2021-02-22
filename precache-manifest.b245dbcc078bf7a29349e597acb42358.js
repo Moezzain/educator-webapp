689e8ffd0cdfbf37c37291db8f764921d84ba1ed
waelvilla <waelvilla@gmail.com>
2021-02-22 18:03:26 +0300

    Updates

---
+++ precache-manifest.b245dbcc078bf7a29349e597acb42358.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81eb72167e6ac1cc88fa0dfc239bc244",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1adb9762b05ee50c3b04",
+    "url": "/static/css/main.66934c36.chunk.css"
+  },
+  {
+    "revision": "c20e607fd10fc636cd66",
+    "url": "/static/js/2.34c60ac9.chunk.js"
+  },
+  {
+    "revision": "70ff602f3418a9899af7d8e228bdac6b",
+    "url": "/static/js/2.34c60ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1adb9762b05ee50c3b04",
+    "url": "/static/js/main.5a674e29.chunk.js"
+  },
+  {
+    "revision": "9362ded47e61cbfa10d6",
+    "url": "/static/js/runtime-main.99c593c9.js"
+  },
+  {
+    "revision": "287167ebcffe0abf05d4c349a0674f58",
+    "url": "/static/media/ithnain.287167eb.png"
+  }
+]);
