20cec1f5a5773775eb38310b511b814fc3babf52
Majed <majedbinomran@gmail.com>
2021-06-24 18:51:11 +0300

    added themes to the app

---
+++ src/styles/themes.js
@@ -0,0 +1,14 @@
+export const mainTheme = {
+  primary: {
+      black:'#042c42',
+      red:'#ee3d60',
+      white:'#ffffff',
+      bluishWhite: '#EDF8FF'
+  },
+  secondary: {
+    lightBlue: '#d2edf4 ',
+    mainBlue: '#51c7ea',
+    darkBlue: '#005792',
+    orange: '#fbbd33'    
+  },
+};
