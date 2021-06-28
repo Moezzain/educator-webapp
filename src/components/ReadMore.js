96c85a30280db1a4a8a5089f63ec3b8e83b43bc6
Majed <majedbinomran@gmail.com>
2021-06-28 16:23:28 +0300

    readmore now has a darkmode text

--- src/components/ReadMore.js
+++ src/components/ReadMore.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-const ReadMore = (note,renderNotes) => {
+import { useSelector } from 'react-redux';
+import { lightStyles, darkStyles } from '../styles/readMoreStyles';
+const ReadMore = ({currentNote,setNotes}) => {
+    const {darkMode } = useSelector((state) => state.auth)
+    const styles = !darkMode ? lightStyles : darkStyles
     return (
         <div>
-            <Button onClick={() => {renderNotes()}}>
+            <Button onClick={() => {setNotes()}}>
                 <ArrowBackIcon/>
             </Button>
-        <div style={{overflow:'auto'}}>
-            {note.text}
+        <div style={styles.text}>
+            {currentNote.text}
         </div>
         </div>
     )
