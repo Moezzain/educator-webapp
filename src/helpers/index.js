06e5aa7592284db6927e029f0a6a6db788d8aed0
Majed <majedbinomran@gmail.com>
2021-06-29 17:52:02 +0300

    added a patientAppointment component with its own styles

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,11 +1,31 @@
 export const concatProfile = (patientProfile, profileValue) => {
-    //getting prfileValue without the last character
-    const valueInside = profileValue.substring(0,profileValue.length-1)
-    let result = '';
-    if(patientProfile?.[profileValue])
-    //concating each value srting to the result 
-  patientProfile[profileValue].forEach((item) => {
-    result = result.concat(item?.[valueInside]+'\n')
-  })
-  return result
-}
+  //getting prfileValue without the last character
+  const valueInside = profileValue.substring(0, profileValue.length - 1);
+  let result = '';
+  if (patientProfile?.[profileValue])
+    //concating each value srting to the result
+    patientProfile[profileValue].forEach((item) => {
+      result = result.concat(item?.[valueInside] + '\n');
+    });
+  return result;
+};
+export const getAppointmentsEducators = (educators, appointments) => {
+  const tempAppointments = [];
+  educators?.forEach((educator) => {
+    tempAppointments.push(
+      appointments
+        .filter((appointment) => {
+          return educator.id === appointment.educatorId;
+        })
+        .map((appointment) => {
+          return {
+            date: appointment.date,
+            educatorId: appointment.educatorId,
+            name: educator.name,
+          };
+        })
+    );
+  });
+
+  return tempAppointments.flat();
+};
