48be05ec12bf6c9d26cf243ae3e5b1df4a407e3b
Majed <majedbinomran@gmail.com>
2021-06-29 17:53:09 +0300

    added title to notes and sumarries

--- src/components/ReadMore.js
+++ src/components/ReadMore.js
@@ -3,19 +3,22 @@ import Button from '@material-ui/core/Button';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { useSelector } from 'react-redux';
 import { lightStyles, darkStyles } from '../styles/readMoreStyles';
-const ReadMore = ({currentNote,setNotes}) => {
-    const {darkMode } = useSelector((state) => state.auth)
-    const styles = !darkMode ? lightStyles : darkStyles
-    return (
-        <div>
-            <Button onClick={() => {setNotes()}}>
-                <ArrowBackIcon/>
-            </Button>
-        <div style={styles.text}>
-            {currentNote.text}
-        </div>
-        </div>
-    )
-}
+const ReadMore = ({ currentNote, setNotes }) => {
+  const { darkMode } = useSelector((state) => state.auth);
+  const styles = !darkMode ? lightStyles : darkStyles;
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          setNotes();
+        }}
+      >
+        <ArrowBackIcon />
+      </Button>
+      <div style={styles.titleText}>{currentNote.title}</div>
+      <div style={styles.text}>{currentNote.text}</div>
+    </div>
+  );
+};
 
-export default ReadMore
+export default ReadMore;
--- src/styles/readMoreStyles.js
+++ src/styles/readMoreStyles.js
@@ -2,8 +2,10 @@ import { mainTheme, darkTheme } from './themes';
 const theme = mainTheme;
 
 export const lightStyles = {
+  titleText: {fontSize:30},
   text: { overflow: 'auto' },
 };
 export const darkStyles = {
+  titleText: {fontSize:30,color: darkTheme.secondary.lightGrey},
   text: { overflow: 'auto', color: darkTheme.secondary.lightGrey },
 };
